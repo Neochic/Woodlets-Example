6cf7a4aa1be4bde573ed5a70d478a3c6825aa884
Christoph Stickel <christoph@neochic.de>
2017-05-11 22:27:35 +0200

    updated tmg config to current woodlets version

--- src/services/Tgm.php
+++ src/services/Tgm.php
@@ -6,7 +6,7 @@ add_action( 'tgmpa_register', function() {
 		array(
 			'name'               => 'Woodlets',
 			'slug'               => 'woodlets',
-			'source'             => 'https://github.com/Neochic/Woodlets/releases/download/v0.5.8/woodlets-v0.5.8-bundled.zip',
+			'source'             => 'https://github.com/Neochic/Woodlets/releases/download/v0.6.0/woodlets-v0.6.0-bundled.zip',
 			'required'           => true,
 			'force_activation'   => true,
 			'force_deactivation' => true
